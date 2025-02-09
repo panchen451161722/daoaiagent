be62d4087a665f25e9330a83df9e13fa01764ce3
mambaCoder <18721021098@163.com>
2025-02-09 21:25:35 +0800

    update contracts

--- contracts/DAOFactory.sol
+++ contracts/DAOFactory.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.0;
 import "./DAOAIAgent.sol"; // 确保DAOAIAgent.sol文件存在并且路径正确
 
 contract DAOFactory {
-    event DAOCreated(address indexed daoAddress, address creator, uint256 timestamp);
+    event DAOCreated(address indexed daoAddress, address creator, uint256 timestamp, bytes32 otherContentHash);
 
     function createDAO(address _tokenContractAddress, bool _allowIndependentAIA, bytes32 _otherContentHash) external {
         DAOAIAgent newDao = new DAOAIAgent(_tokenContractAddress, _allowIndependentAIA, _otherContentHash);
-        emit DAOCreated(address(newDao), msg.sender, block.timestamp);
+        emit DAOCreated(address(newDao), msg.sender, block.timestamp, _otherContentHash);
     }
 }
+
